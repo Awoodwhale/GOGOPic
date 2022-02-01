380c5b6b4856474fdd4d2ac126fbc363f3dee3ba
woodwhale <woodwhale@qq.com>
2022-02-01 22:23:11 +0800

    更新了主页的UI界面

--- app/src/main/java/top/woodwhale/gogopic/model/domain/API.java
+++ app/src/main/java/top/woodwhale/gogopic/model/domain/API.java
@@ -3,11 +3,13 @@ package top.woodwhale.gogopic.model.domain;
 import java.util.Map;
 
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
+import retrofit2.http.QueryMap;
 import top.woodwhale.gogopic.utils.UrlUtils;
 
 public interface API {
@@ -16,4 +18,7 @@ public interface API {
 
     @GET(UrlUtils.CATEGORIES_URL)
     Call<Categories> getCategories(@HeaderMap Map<String, String> map);
+
+    @GET(UrlUtils.COMICS_URL)
+    Call<ResponseBody> getComics(@HeaderMap Map<String, String> headerMap, @QueryMap Map<String,String> queryMap);
 }
--- app/src/main/java/top/woodwhale/gogopic/ui/fragment/HomeFragment.java
+++ app/src/main/java/top/woodwhale/gogopic/ui/fragment/HomeFragment.java
@@ -1,55 +1,64 @@
 package top.woodwhale.gogopic.ui.fragment;
 
 import android.annotation.SuppressLint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.Unbinder;
 import top.woodwhale.gogopic.R;
 import top.woodwhale.gogopic.model.domain.Categories;
 import top.woodwhale.gogopic.presenter.IHomePresent;
 import top.woodwhale.gogopic.presenter.impl.HomePresentImpl;
+import top.woodwhale.gogopic.ui.adapter.HomeContentAdapter;
 import top.woodwhale.gogopic.view.IHomeCallback;
 
 @SuppressLint("NonConstantResourceId")
 public class HomeFragment extends Fragment implements IHomeCallback {
 
     @BindView(R.id.base_container) protected FrameLayout mBaseContainer;
-    @BindView(R.id.bt_test) protected Button mButton;
+    @BindView(R.id.home_content_list) protected RecyclerView mHomeContent;
     private Unbinder mBind;
     private IHomePresent mHomePresent;
-
-    @OnClick({R.id.bt_test})
-    public void test(View v) {
-        if (v.getId() == R.id.bt_test) {
-            loadData();
-        }
-    }
+    private View mRootView;
+    private HomeContentAdapter mHomeContentAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = loadRootView(inflater,container);
-        mBind = ButterKnife.bind(this, rootView);
+        mRootView = loadRootView(inflater,container);
+        mBind = ButterKnife.bind(this, mRootView);
 
-        initView(rootView);
+        initView(mRootView);
         initPresenter();
         loadData();
-        return rootView;
+        return mRootView;
     }
 
     private void initView(View rootView) {
+        mHomeContent.setLayoutManager(new GridLayoutManager(getContext(),3));
+        mHomeContent.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                outRect.top = 9;
+                outRect.bottom = 9;
+            }
+        });
+        // 创建适配器
+        mHomeContentAdapter = new HomeContentAdapter();
+        // 设置适配器
+        mHomeContent.setAdapter(mHomeContentAdapter);
     }
 
     private void initPresenter() {
@@ -82,6 +91,6 @@ public class HomeFragment extends Fragment implements IHomeCallback {
 
     @Override
     public void onCategoriesLoaded(Categories categories) {
-
+        mHomeContentAdapter.setData(categories);
     }
 }
